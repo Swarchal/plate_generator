533028bcf845f2c6878838fda6ed326bdc81c08e
Swarchal <scott.warchal@gmail.com>
2019-02-07 12:08:30 +0000

    init repo

---
+++ plate_generator/__init__.py
@@ -0,0 +1,1 @@
+from plate_generator.plate_generator import *
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name="plate_generator",
+    packages=["plate_generator"],
+    install_requires=["np"]
+)
