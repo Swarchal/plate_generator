40248ece6c27313529546822a20f7e315d45cbad
Swarchal <scott.warchal@gmail.com>
2019-02-07 13:18:41 +0000

    fix numpy name

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup
 setup(
     name="plate_generator",
     packages=["plate_generator"],
-    install_requires=["np"]
+    install_requires=["numpy"],
+    tests_require=["pytest"]
 )
