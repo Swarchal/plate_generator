d262c11c47dd276728b50db08e86782f9e98fdb9
Swarchal <scott.warchal@gmail.com>
2019-04-08 13:11:18 +0200

    add scipy as dependency

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name="plate_generator",
     packages=["plate_generator"],
-    install_requires=["numpy"],
+    install_requires=["numpy", "scipy"],
     tests_require=["pytest"]
 )
+
