b5f2d3d26b9609c809fc2814ec4aea8c533b9d92
Scott Warchal <scott.warchal@gmail.com>
2020-04-16 11:49:05 +0200

    add license

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@ from setuptools import setup
 
 setup(
     name="plate_generator",
+    author="Scott Warchal",
     packages=["plate_generator"],
     install_requires=["numpy", "scipy"],
+    license="MIT",
     tests_require=["pytest"],
 )
